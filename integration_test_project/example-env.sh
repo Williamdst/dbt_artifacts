5d803756c8caea35c04b15b0377303af834d7754
Denzel S. Williams <49127460+Williamdst@users.noreply.github.com>
2023-06-05 09:58:55 -0400

    feat: Remove non-Snowflake variables

--- integration_test_project/example-env.sh
+++ integration_test_project/example-env.sh
@@ -10,12 +10,6 @@ export DBT_ENV_SECRET_SNOWFLAKE_TEST_PASSWORD=
 export DBT_ENV_SECRET_SNOWFLAKE_TEST_ROLE=
 export DBT_ENV_SECRET_SNOWFLAKE_TEST_DATABASE=
 export DBT_ENV_SECRET_SNOWFLAKE_TEST_WAREHOUSE=
-export DBT_ENV_SECRET_DATABRICKS_HOST=
-export DBT_ENV_SECRET_DATABRICKS_HTTP_PATH=
-export DBT_ENV_SECRET_DATABRICKS_TOKEN=
-export DBT_ENV_SECRET_GCP_PROJECT=
-export DBT_ENV_SPARK_DRIVER_PATH= # /Library/simba/spark/lib/libsparkodbc_sbu.dylib on a Mac
-export DBT_ENV_SPARK_ENDPOINT= # The endpoint ID from the Databricks HTTP path
 
 # dbt environment variables, change these
 export DBT_CLOUD_PROJECT_ID=
